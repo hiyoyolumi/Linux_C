6a995ffd3c8849362754d24c05a6566443286b56
crushbb <862127279@qq.com>
2021-04-11 20:15:58 +0800

    getpid

---
+++ Myshell/getpid.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+int main()
+{
+    pid_t  pid;
+
+    if((pid = fork()) == -1)
+    {
+        printf("fork error!\n");
+        exit(1);
+    }
+
+    if(pid == 0)
+    {
+        printf("getpid return %d\n", getpid());
+    }
+    
+    exit(0);
+}
